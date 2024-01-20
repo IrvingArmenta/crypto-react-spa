079bf7f0caf50923619fa133d1284ef2d2744a5b
IrvingArmenta <irvingarmentajd@gmail.com>
2024-01-20 13:57:12 +0900

    feature: initial environment setup of eslint - jest and vite

--- jest.config.ts
+++ jest.config.ts
@@ -1,24 +1,13 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   // globalSetup: '<rootDir>/test/globalSetup.ts',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.mjs$': 'babel-jest'
+    '^.+\\.tsx?$': 'ts-jest'
   },
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     '\\.(css|scss)$': 'identity-obj-proxy',
-    '@/(.*)$': '<rootDir>/src/$1',
-    '@style/(.*)$': '<rootDir>/styled-system/$1'
-  },
-  coveragePathIgnorePatterns: [
-    'node_modules',
-    '<rootDir>/src/main.tsx',
-    'styled-system',
-    'api.develop.ts',
-    '<rootDir>/src/views/index.tsx'
-  ],
-  prettierPath: require.resolve('prettier-2')
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 };
