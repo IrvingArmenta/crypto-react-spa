6b0cd3e32e91ea38c638ebe136d50e9a3bfaedfa
IrvingArmenta <irvingarmentajd@gmail.com>
2024-01-24 00:37:47 +0900

    refactor: cleaning up some code in api

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,25 +1,29 @@
 import { currencyFormat } from '@/utils/currencyFormat';
 import { GetPriceUriFrontendReturnType, GetPriceUriReturnType } from './types';
-import { fetchHeaderWithKey, GET_SIMPLE_PRICE_URI } from './config';
+import { config } from './config';
 
 export const fetchSimplePrices = async () => {
-  const response = await fetch(GET_SIMPLE_PRICE_URI, fetchHeaderWithKey);
+  const response = await fetch(
+    config.GET_SIMPLE_PRICE_URI,
+    config.FETCH_HEADER
+  );
   const json = (await response.json()) as GetPriceUriReturnType | undefined;
 
   if (!json) {
     throw new Error(
-      `Something went wrong with ${GET_SIMPLE_PRICE_URI} request`
+      `Something went wrong with ${config.GET_SIMPLE_PRICE_URI} request`
     );
   }
 
+  const { bitcoin, ethereum } = json;
   const frontEndReturn: GetPriceUriFrontendReturnType = {
     btc: {
-      price: currencyFormat(json.bitcoin.usd),
-      percentageDiff24h: `${json.bitcoin.usd_24h_change.toFixed(2)}%`
+      price: currencyFormat(bitcoin.usd),
+      percentageDiff24h: `${bitcoin.usd_24h_change.toFixed(2)}%`
     },
     eth: {
-      price: currencyFormat(json.ethereum.usd),
-      percentageDiff24h: `${json.ethereum.usd_24h_change.toFixed(2)}%`
+      price: currencyFormat(ethereum.usd),
+      percentageDiff24h: `${ethereum.usd_24h_change.toFixed(2)}%`
     }
   };
 
