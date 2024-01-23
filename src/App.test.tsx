efc1f3b6e99bbe4a14d62aafb6c7574740501a08
IrvingArmenta <irvingarmentajd@gmail.com>
2024-01-24 00:37:47 +0900

    refactor: cleaning up some code in api

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,12 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
 describe('App Tests', () => {
-  it('Renders the main page', () => {
+  it('Renders the main page', async () => {
     render(<App />);
+
+    await waitFor(() => screen);
 
     expect(screen).toBeTruthy();
   });
