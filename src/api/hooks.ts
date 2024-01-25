8102a0c29c8f3c68d44b24402a42a1eba1ab32c3
IrvingArmenta <irvingarmentajd@gmail.com>
2024-01-25 11:46:36 +0900

    feat: api improvements for fetching ohlc data and other configs

--- src/api/hooks.ts
+++ src/api/hooks.ts
@@ -1,11 +1,23 @@
 import useSWR from 'swr';
 import { GET_SIMPLE_PRICE_URI } from './config';
-import { fetchSimplePrices } from './index';
+import { fetchOHLC, fetchSimplePrices } from './index';
+import type { CoinIdType } from '@/global-types';
 
-export function useGetSimplePrice() {
-  const { data, isLoading, isValidating } = useSWR(
+export function useGetSimplePriceData() {
+  const { data, isLoading, isValidating, error } = useSWR(
     GET_SIMPLE_PRICE_URI,
     fetchSimplePrices
+  );
+
+  return { data, isLoading, isValidating, error: error as Error };
+}
+
+export function useGetOHLCData(coinId: CoinIdType) {
+  const fetchKey = `get/ohlc/${coinId}`;
+
+  const { data, isLoading, isValidating } = useSWR(
+    [fetchKey, coinId],
+    ([, coinId]) => fetchOHLC(coinId)
   );
 
   return { data, isLoading, isValidating };
