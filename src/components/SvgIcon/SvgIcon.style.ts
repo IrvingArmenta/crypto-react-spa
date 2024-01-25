2f6e386649574184f3a7d638efd83e81d3dd9186
IrvingArmenta <irvingarmentajd@gmail.com>
2024-01-25 11:47:46 +0900

    style: new svg logo icons and some updates for component

--- src/components/SvgIcon/SvgIcon.style.ts
+++ src/components/SvgIcon/SvgIcon.style.ts
@@ -1,10 +1,11 @@
 import { css } from '@style/css';
 
-export const svgIconRawStyle = css.raw({
+export const svgIconStyle = css({
   color: '#333',
   aspectRatio: 1,
   width: '1rem',
   display: 'inline-flex',
   alignItems: 'center',
-  justifyContent: 'center'
+  justifyContent: 'center',
+  pointerEvents: 'none'
 });
